f9f22f95567b237a08e05728f145a8e6d2675f13
a.karimi <a.karimi@cloud.ir>
2025-02-05 13:27:50 +0330

    patch : service provide r one line

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,7 +27,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        $keyword = ['technology', 'sports', 'health', 'business', 'entertainment', 'science', 'politics', 'finance'][array_rand(['technology', 'sports', 'health', 'business', 'entertainment', 'science', 'politics', 'finance'])];
-//        AggregateArticlesJob::dispatch($keyword);
+        AggregateArticlesJob::dispatch(['technology', 'sports', 'health', 'business', 'entertainment', 'science', 'politics', 'finance'][array_rand(['technology', 'sports', 'health', 'business', 'entertainment', 'science', 'politics', 'finance'])]);
     }
 }
