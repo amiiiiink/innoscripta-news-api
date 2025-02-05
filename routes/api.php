e2dc2a6f56232bbe214a33270ddd76cc6a1a7c97
a.karimi <a.karimi@cloud.ir>
2025-02-05 13:49:36 +0330

    fix : change auth controller structure

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\Api\V1\ArticleController;
+use App\Http\Controllers\Api\V1\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/auth/token', [AuthController::class, 'generateToken'])->name('login');
